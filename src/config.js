f633e013451def063e30066e6dd9e7f0966a7e01
taylorsdugger <taylorsdugger@gmail.com>
2018-12-22 12:32:10 -0600

    update configs

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 const config = {
     api: "",
     auth: {
-        siteDomain: 'https://duggerswhiskey.com',
+        siteDomain: 'https://duggerswhiskey.net/',
         domain: 'duggerswhiskey.auth0.com',
         clientID: 'R600OJ9xOLh6mRWGgGNG5YCSS3oVHEnu',
-        redirectUri: process.env.AUTH_REDIRECT_URL || 'http://localhost:3000/callback'
+        redirectUri: process.env.AUTH_REDIRECT_URL || 'https://duggerswhiskey.net/callback'
     }
 };
 export default config;
