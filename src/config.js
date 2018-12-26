125fa9891c9001b6b059ca384e53c1787d9f4d9f
taylorsdugger <taylorsdugger@gmail.com>
2018-12-26 12:12:57 -0600

    token

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ const config = {
         siteDomain: 'https://duggerswhiskey.net/',
         domain: 'duggerswhiskey.auth0.com',
         clientID: 'R600OJ9xOLh6mRWGgGNG5YCSS3oVHEnu',
-        redirectUri: process.env.AUTH_REDIRECT_URL || 'https://duggerswhiskey.net/callback'
+        redirectUri: '#{AUTH_REDIRECT_URL}#'// || 'https://duggerswhiskey.net/callback'
     }
 };
 export default config;
